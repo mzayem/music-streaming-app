94353b2382f333c24baf583d3d5e0539465702f3
mzayem <mzayemazam@gmail.com>
2024-08-09 18:02:19 +0500

    setting up debounce

---
+++ components/searchInput.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function SearchInput() {
+  return (
+    <div className="flex flex-col gap-y-2">
+      <input
+        className="bg-neutral-800 rounded-lg p-2 text-white"
+        placeholder="Search"
+      />
+    </div>
+  );
+}
+2;
