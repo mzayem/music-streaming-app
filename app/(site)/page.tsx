47607b17c5fa928161d9c05319f8a6b0cb2e4feb
mzayem <mzayemazam@gmail.com>
2024-08-09 14:19:31 +0500

    list styling

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,13 @@
+import getSongs from "@/actions/getSong";
 import Header from "@/components/header";
 import ListItem from "@/components/listitem";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div
       className="
@@ -48,7 +52,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold"> Newest songs</h1>
         </div>
-        <div>List of songs</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
