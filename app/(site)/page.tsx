d916e1f4328f6bc1d6b61f7aef629f5e902d14fa
mzayem <mzayemazam@gmail.com>
2024-08-09 11:43:43 +0500

    creating song fetch system

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/header";
 import ListItem from "@/components/listitem";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div
@@ -46,7 +48,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold"> Newest songs</h1>
         </div>
-        {/* <div>List of songs</div> */}
+        <div>List of songs</div>
       </div>
     </div>
   );
