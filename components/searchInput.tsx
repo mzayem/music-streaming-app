f89d04e4bf7ce077964790cba031b3d9a5efa7ff
mzayem <mzayemazam@gmail.com>
2024-08-09 18:05:02 +0500

    working

--- components/searchInput.tsx
+++ components/searchInput.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function SearchInput() {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
   return (
     <div className="flex flex-col gap-y-2">
       <input
