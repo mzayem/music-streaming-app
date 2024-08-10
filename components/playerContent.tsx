18ff69383f12b7e7c764774bf4619db876ca8ffb
mzayem <mzayemazam@gmail.com>
2024-08-10 20:29:39 +0500

    working on player

--- components/playerContent.tsx
+++ components/playerContent.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { Song } from "@/types";
-import MediaItem from "./mediaItem";
-import LikeButton from "./likeButton";
+
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import usePlayer from "@/hooks/usePlayer";
+
+import MediaItem from "./mediaItem";
+import LikeButton from "./likeButton";
 import Slider from "./slider";
+import { useState } from "react";
 
 interface PlayerContentProps {
   song: Song;
@@ -14,8 +18,42 @@ interface PlayerContentProps {
 }
 
 export default function PlayerContent({ song, songUrl }: PlayerContentProps) {
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  };
+
+  const onPlayPrev = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const prevSong = player.ids[currentIndex - 1];
+
+    if (!prevSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(prevSong);
+  };
 
   return (
     <div
@@ -79,7 +117,7 @@ export default function PlayerContent({ song, songUrl }: PlayerContentProps) {
       "
       >
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrev}
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
@@ -100,7 +138,7 @@ export default function PlayerContent({ song, songUrl }: PlayerContentProps) {
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
@@ -114,7 +152,7 @@ export default function PlayerContent({ song, songUrl }: PlayerContentProps) {
           justify-end
           pr-2"
       >
-        <div className="flex items-center gap-x-2 w-[]">
+        <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
             size={34}
             className="text-neutral-400 cursor-pointer hover:text-white transition"
