6243dc5d95f191da4695d7dd8006e4086d71a6b5
mzayem <mzayemazam@gmail.com>
2024-10-27 20:33:31 +0500

    stripe client setup

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,5 +1,11 @@
-// import Stripe from "stripe";
+import Stripe from "stripe";
 
-// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-//   apiVersion: "2022-11-15",
-// });
+export const stripe = new Stripe(
+    process.env.STRIPE_SECRET_KEY ?? '', 
+    {
+    apiVersion: "2024-06-20",
+   appInfo: {
+       name: "Beat Stream",
+       version: "1.0.0", 
+   }
+});
